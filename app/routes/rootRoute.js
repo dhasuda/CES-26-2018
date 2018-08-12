2c3f4128550a1b5bc78fb010c56507933cbcc8bd
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-08-11 22:32:37 -0300

    Adding mongo

--- app/routes/rootRoute.js
+++ app/routes/rootRoute.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
+const mongo = require('mongodb')
+
 const rootController = require('../controllers/rootController.js')
 
 router.get('/', rootController.firstPage)
