a1376d1c798b701c5d856fb0e37e31514c71b581
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-09-15 23:09:20 -0300

    Adding Rank save

---
+++ app/models/rank.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/"
+
+var Rank = (data) => {
+    this.data = data
+}
+
+Rank.prototype.data = {}
+
+Rank.prototype.save = (onSuccess, onError) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("nodeData");
+    
+        dbo.collection("ranks").insertOne(this.data, function(err, res) {
+          if (err) {
+            onError(err)
+            throw err
+          } else {
+            console.log("1 rank inserted");
+            db.close()
+            onSuccess()
+          }
+        })
+      })
+}
